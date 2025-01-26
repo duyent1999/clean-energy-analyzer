efe04c3dca68373736ecf533388cbed42adaaf48
duyen <duyentran357@gmail.com>
2025-01-25 20:29:22 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,11 +13,6 @@ output "lambda_role_arn" {
   value       = aws_iam_role.lambda_exec_role.arn
 }
 
-output "lambda_function_name" {
-  description = "The name of the Lambda function"
-  value       = aws_lambda_function.weather_lambda.function_name
-}
-
 output "lambda_function_arn" {
   description = "The ARN of the Lambda function"
   value       = aws_lambda_function.weather_lambda.arn
