23ea534218de923c179fbe9f45b1ce8bcab0d0b1
duyen <duyentran357@gmail.com>
2025-01-26 13:08:04 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,11 +33,61 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-# Attach the basic execution policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# IAM Policy for Lambda to access S3, CloudWatch and ECR
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "Policy for Lambda to access S3, CloudWatch, and ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # S3 Permissions
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.my_bucket.arn,
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
+      },
+      # CloudWatch Logs Permissions
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      # ECR Permissions
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
+
+# Attach the IAM Policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
 
 resource "aws_lambda_function" "clean_energy_lambda" {
   function_name = "clean-energy-lambda"
@@ -58,44 +108,6 @@ resource "aws_lambda_function" "clean_energy_lambda" {
   }
 }
 
-# IAM Policy for Lambda to access S3 and CloudWatch
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "Policy for Lambda to access S3 and CloudWatch"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.my_bucket.arn,
-          "${aws_s3_bucket.my_bucket.arn}/*"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach the IAM Policy to the IAM Role
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
 
 # S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
