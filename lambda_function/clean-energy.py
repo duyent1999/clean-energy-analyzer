0959271408e31e6e66a76bc9e659fb66146d8628
duyen <duyentran357@gmail.com>
2025-01-28 12:00:36 -0500

    Update clean-energy.py

--- lambda_function/clean-energy.py
+++ lambda_function/clean-energy.py
@@ -28,7 +28,8 @@ def lambda_handler(event, context):
         if not api_key:
             raise Exception("secret not found in secrets")
 
-        city = event['queryStringParameters']['city'] if event['queryStringParameters'] else 'New York'
+        #city = event['queryStringParameters']['city'] if event['queryStringParameters'] else 'New York'
+        city = event.get('city', 'New York') 
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
         # Make the API request
